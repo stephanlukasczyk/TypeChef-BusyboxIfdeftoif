631ea44df90ea06dd9706b38aa525a33089c7924
Florian Garbe <fgarbe@fim.uni-passau.de>
2014-05-05 01:20:22 +0200

    changed jcpp.sh to use ./ifdeftoif.sh

--- jcpp.sh
+++ jcpp.sh
@@ -37,7 +37,7 @@ shift
 # though!
 echo "==TypeChef $inp"
 
-bash -c "time $typechefDir/typechef.sh\
+bash -c "time $typechefDir/ifdeftoif.sh\
   $(for arg in $partialPreprocFlags "$@"; do echo -n "\"$arg\" "; done) \
   '$inp' 2> '$outErr' |tee '$outDbg'" \
   2> "$outTime" || true
