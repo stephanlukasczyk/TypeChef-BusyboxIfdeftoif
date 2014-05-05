baccba33ec2e290ebe4d85cbd220476f9578fa28
Florian Garbe <fgarbe@fim.uni-passau.de>
2014-05-05 02:00:06 +0200

    updated gitignore, renamed preparescript

--- prepareBusybox.sh
+++ prepareBusybox.sh
@@ -1,9 +1,42 @@
-#this script creates the necessary .pc files describing file presence conditions.
-#
-#run "sbt mkrun" to create run.sh the first time
-#(potentially you will need to compile and publish the main TypeChef project first with "sbt publish-local")
-#
-#create a directory  busybox-1.18.5 and download busybox sources there
+#!/bin/bash
+srcPath="busybox-1.18.5"
+srcPathArchive="busybox-1.18.5.tar.bz2"
+mkRun="run.sh"
+systemSourceArchive="includes-redhat.tar.bz2"
+systemSourceFolder="systems/redhat/usr"
+redhatFolder="systems/redhat"
 
+
+# Check for busybox source files
+if [ ! -d $srcPath ]; then
+	# Busybox directory does not exist
+	if [ ! -f $srcPathArchive ]; then
+		#Download source files
+		echo "Downloading BusyBox source files."
+		wget http://www.busybox.net/downloads/busybox-1.18.5.tar.bz2
+	fi
+	# Extract source files
+	tar xvjf busybox-1.18.5.tar.bz2
+fi
+
+# Check for run.sh file
+if [ ! -f $mkRun ]; then
+	java -Xmx1024M -Xss256M -XX:PermSize=256M -XX:MaxPermSize=512M -jar sbt-launch.jar mkrun
+fi
+# Prepare busybox script
 ./run.sh de.fosd.typechef.busybox.ProcessFileList busybox/busybox_pcs.txt busybox-1.18.5/
 
+# Check system source files
+if [ ! -d $systemSourceFolder ]; then
+	if [ ! -f $systemSourceArchive ]; then
+		#Download system files
+		echo "Downloading system source files."
+		wget http://www.cs.cmu.edu/%7Eckaestne/tmp/includes-redhat.tar.bz2
+	fi
+	# Create redhat folder
+	if [ ! -d $redhatFolder ]; then
+		mkdir -p $redhatFolder
+	fi
+	# Extract system files
+	tar xvjf $systemSourceArchive -C $redhatFolder
+fi
