3a78b881cc423442bcacbc052471cf50f08fe736
Florian Garbe <fgarbe@fim.uni-passau.de>
2014-05-29 14:04:42 +0200

    added different scripts to transform a single file [ ./transformOne.sh bbunzip ] for example

---
+++ transAndReplace.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+#!/bin/bash -vxe
+
+filesToProcess() {
+  local listFile=busybox/busybox_files
+  cat $listFile
+  #awk -F: '$1 ~ /.c$/ {print gensub(/\.c$/, "", "", $1)}' < linux_2.6.33.3_pcs.txt
+}
+
+path=$(cd "$(dirname "$0")"; pwd)
+extension="_ifdeftoif.c"
+flags="-U HAVE_LIBDMALLOC -DCONFIG_FIND -U CONFIG_FEATURE_WGET_LONG_OPTIONS -U ENABLE_NC_110_COMPAT -U CONFIG_EXTRA_COMPAT -D_GNU_SOURCE"
+srcPath="busybox-1.18.5"
+filePath=$(find $srcPath -name $1.c |head -1)
+baseFilePath="${filePath%.c}"
+ifdeftoifPath="${baseFilePath}_ifdeftoif.c"
+renamedPath="${baseFilePath}_orig.c"
+export partialPreprocFlags="--bdd -x CONFIG_ --include busybox/config.h -I $srcPath/include --featureModelDimacs busybox/featureModel.dimacs --recordTiming --parserstatistics --debugInterface --ifdeftoifstatistics"
+
+## Reset output
+echo "Starting process on file $1"
+./jcpp.sh $filePath $flags
+
+## Create id2i_optionstruct
+./../TypeChef/ifdeftoif.sh --featureConfig BusyBoxDefConfig.config
+
+## Rename files
+mv "$filePath" "$renamedPath"
+mv "$ifdeftoifPath" "$filePath"
