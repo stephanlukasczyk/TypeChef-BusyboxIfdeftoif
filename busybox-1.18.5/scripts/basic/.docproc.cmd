3ab8e6ea0898da7a73164d1a13683bd1caf25773
Florian Garbe <fgarbe@fim.uni-passau.de>
2014-05-30 11:45:04 +0200

    busybox finally

--- busybox-1.18.5/scripts/basic/.docproc.cmd
+++ busybox-1.18.5/scripts/basic/.docproc.cmd
@@ -2,72 +2,57 @@ cmd_scripts/basic/docproc := gcc -Wp,-MD,scripts/basic/.docproc.d  -Wall -Wstric
 
 deps_scripts/basic/docproc := \
   scripts/basic/docproc.c \
-  /usr/include/stdc-predef.h \
   /usr/include/stdio.h \
-  /usr/include/features.h \
-  /usr/include/x86_64-linux-gnu/sys/cdefs.h \
-  /usr/include/x86_64-linux-gnu/bits/wordsize.h \
-  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
-  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
-  /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stddef.h \
-  /usr/include/x86_64-linux-gnu/bits/types.h \
-  /usr/include/x86_64-linux-gnu/bits/typesizes.h \
-  /usr/include/libio.h \
-  /usr/include/_G_config.h \
-  /usr/include/wchar.h \
-  /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdarg.h \
-  /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
-  /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
-  /usr/include/x86_64-linux-gnu/bits/stdio.h \
-  /usr/include/x86_64-linux-gnu/bits/stdio2.h \
+  /usr/include/sys/cdefs.h \
+  /usr/include/sys/_symbol_aliasing.h \
+  /usr/include/sys/_posix_availability.h \
+  /usr/include/Availability.h \
+  /usr/include/AvailabilityInternal.h \
+  /usr/include/_types.h \
+  /usr/include/sys/_types.h \
+  /usr/include/machine/_types.h \
+  /usr/include/i386/_types.h \
+  /usr/include/secure/_stdio.h \
+  /usr/include/secure/_common.h \
   /usr/include/stdlib.h \
-  /usr/include/x86_64-linux-gnu/bits/waitflags.h \
-  /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
-  /usr/include/endian.h \
-  /usr/include/x86_64-linux-gnu/bits/endian.h \
-  /usr/include/x86_64-linux-gnu/bits/byteswap.h \
-  /usr/include/x86_64-linux-gnu/bits/byteswap-16.h \
-  /usr/include/x86_64-linux-gnu/sys/types.h \
-  /usr/include/time.h \
-  /usr/include/x86_64-linux-gnu/sys/select.h \
-  /usr/include/x86_64-linux-gnu/bits/select.h \
-  /usr/include/x86_64-linux-gnu/bits/sigset.h \
-  /usr/include/x86_64-linux-gnu/bits/time.h \
-  /usr/include/x86_64-linux-gnu/bits/select2.h \
-  /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
-  /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+  /usr/include/sys/wait.h \
+  /usr/include/sys/signal.h \
+  /usr/include/sys/appleapiopts.h \
+  /usr/include/machine/signal.h \
+  /usr/include/i386/signal.h \
+  /usr/include/i386/_structs.h \
+  /usr/include/sys/_structs.h \
+  /usr/include/machine/_structs.h \
+  /usr/include/mach/i386/_structs.h \
+  /usr/include/sys/resource.h \
+  /usr/include/machine/endian.h \
+  /usr/include/i386/endian.h \
+  /usr/include/sys/_endian.h \
+  /usr/include/libkern/_OSByteOrder.h \
+  /usr/include/libkern/i386/_OSByteOrder.h \
   /usr/include/alloca.h \
-  /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
-  /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
-  /usr/include/x86_64-linux-gnu/bits/stdlib.h \
+  /usr/include/machine/types.h \
+  /usr/include/i386/types.h \
+  /usr/include/i386/_types.h \
   /usr/include/string.h \
-  /usr/include/xlocale.h \
-  /usr/include/x86_64-linux-gnu/bits/string.h \
-  /usr/include/x86_64-linux-gnu/bits/string2.h \
-  /usr/include/x86_64-linux-gnu/bits/string3.h \
+  /usr/include/strings.h \
+  /usr/include/secure/_string.h \
   /usr/include/ctype.h \
+  /usr/include/runetype.h \
   /usr/include/unistd.h \
-  /usr/include/x86_64-linux-gnu/bits/posix_opt.h \
-  /usr/include/x86_64-linux-gnu/bits/environments.h \
-  /usr/include/x86_64-linux-gnu/bits/confname.h \
-  /usr/include/getopt.h \
-  /usr/include/x86_64-linux-gnu/bits/unistd.h \
-  /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed/limits.h \
-  /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed/syslimits.h \
+  /usr/include/sys/unistd.h \
+  /usr/include/sys/select.h \
+  /usr/include/sys/_select.h \
+  /usr/llvm-gcc-4.2/bin/../lib/gcc/i686-apple-darwin11/4.2.1/include/limits.h \
+  /usr/llvm-gcc-4.2/bin/../lib/gcc/i686-apple-darwin11/4.2.1/include/syslimits.h \
+  /Applications/Xcode.app/Contents/Developer/usr/llvm-gcc-4.2/lib/gcc/i686-apple-darwin11/4.2.1/include/limits.h \
+  /Applications/Xcode.app/Contents/Developer/usr/llvm-gcc-4.2/lib/gcc/i686-apple-darwin11/4.2.1/include/syslimits.h \
   /usr/include/limits.h \
-  /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
-  /usr/include/x86_64-linux-gnu/bits/local_lim.h \
-  /usr/include/linux/limits.h \
-  /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
-  /usr/include/x86_64-linux-gnu/sys/wait.h \
-  /usr/include/signal.h \
-  /usr/include/x86_64-linux-gnu/bits/signum.h \
-  /usr/include/x86_64-linux-gnu/bits/siginfo.h \
-  /usr/include/x86_64-linux-gnu/bits/sigaction.h \
-  /usr/include/x86_64-linux-gnu/bits/sigcontext.h \
-  /usr/include/x86_64-linux-gnu/bits/sigstack.h \
-  /usr/include/x86_64-linux-gnu/sys/ucontext.h \
-  /usr/include/x86_64-linux-gnu/bits/sigthread.h \
+  /usr/include/machine/limits.h \
+  /usr/include/i386/limits.h \
+  /usr/include/i386/_limits.h \
+  /usr/include/sys/syslimits.h \
+  /usr/include/sys/types.h \
 
 scripts/basic/docproc: $(deps_scripts/basic/docproc)
 
