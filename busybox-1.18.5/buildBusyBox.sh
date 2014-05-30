48e08a27b93c8886d89cd7921b619475a29feab6
Florian Garbe <f.garbe@gmx.de>
2014-05-30 10:47:45 +0200

    busybox

---
+++ busybox-1.18.5/buildBusyBox.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+timeout 200 make -j 4
+
+RETVAL=$?
+[ $RETVAL -eq 0 ] && echo "Success_Build"
+[ $RETVAL -eq 124 ] && echo "Failure_Timeout"
+[ $RETVAL -ne 0 ] && echo "Failure_Build"
+
+exit
