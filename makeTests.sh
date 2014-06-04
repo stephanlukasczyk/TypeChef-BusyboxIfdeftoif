b447882ef8347b332fd50363ec9a16ed1531c14b
Florian Garbe <fgarbe@fim.uni-passau.de>
2014-06-04 23:41:35 +0200

    scripts changed and added

---
+++ makeTests.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -e
+#!/bin/bash -vxe
+
+filesToProcess() {
+  local listFile=busybox/busybox_files
+  cat $listFile
+  #awk -F: '$1 ~ /.c$/ {print gensub(/\.c$/, "", "", $1)}' < linux_2.6.33.3_pcs.txt
+}
+
+path=$(cd "$(dirname "$0")"; pwd)
+extension="_ifdeftoif.c"
+flags="-U HAVE_LIBDMALLOC -DCONFIG_FIND -U CONFIG_FEATURE_WGET_LONG_OPTIONS -U ENABLE_NC_110_COMPAT -U CONFIG_EXTRA_COMPAT -D_GNU_SOURCE"
+srcPath="busybox-1.18.5"
+testOutputPath="test_results"
+export partialPreprocFlags="--bdd -x CONFIG_ --include busybox/config.h -I $srcPath/include --featureModelDimacs busybox/featureModel.dimacs --recordTiming --parserstatistics --debugInterface --ifdeftoifstatistics"
+
+if [ ! -d $testOutputPath ]; then
+	mkdir $testOutputPath
+fi
+
+## Reset output
+filesToProcess|while read i; do
+  if [ ! -f $srcPath/$i$extension ]; then
+# this script run transforms the input C file using ifdeftoif transformations
+    ./jcpp.sh $srcPath/$i.c $flags
+	
+# Create id2i_optionstruct
+	./../TypeChef/ifdeftoif.sh --featureConfig BusyBoxDefConfig.config
+# Swap original and transformed file
+	mv "$srcPath/$i.c" "$srcPath/${i}_orig.c"
+	mv "$srcPath/${i}_ifdeftoif.c" "$srcPath/$i.c"
+	cd $srcPath
+# Start making busybox
+	echo "-=Make=- $srcPath/$i.c"
+	make > ../$testOutputPath/$i.make 2>&1
+	cd testsuite
+# Start testing busybox
+	echo "-=Test=- $srcPath/$i.c"
+	./runtest > ../$testOutputPath/$i.test 2>&1
+	cd $path
+# Swap files back to original state
+	mv "$srcPath/$i.c" "$srcPath/${i}_ifdeftoif.c"
+	mv "$srcPath/${i}_orig.c" "$srcPath/$i.c"
+	git reset --hard HEAD
+	echo "-=Done=- $srcPath/$i.c"
+  else
+    echo "Skipping $srcPath/$i.c"
+  fi
+done
--- total_cleanBusybox.sh
+++ total_cleanBusybox.sh
@@ -1,9 +1,9 @@
-find busybox-1.18.5/ -name "*.dbg*" |while read i; do echo $i; rm $i; done 
-find busybox-1.18.5/ -name "*.err" |while read i; do echo $i; rm $i; done 
-find busybox-1.18.5/ -name "*.interface" |while read i; do echo $i; rm $i; done 
+#find busybox-1.18.5/ -name "*.dbg*" |while read i; do echo $i; rm $i; done 
+#find busybox-1.18.5/ -name "*.err" |while read i; do echo $i; rm $i; done 
+#find busybox-1.18.5/ -name "*.interface" |while read i; do echo $i; rm $i; done 
 find busybox-1.18.5/ -name "*.pi" |while read i; do echo $i; rm $i; done 
 find busybox-1.18.5/ -name "*.pi_dbg" |while read i; do echo $i; rm $i; done
-find busybox-1.18.5/ -name "*.time" |while read i; do echo $i; rm $i; done 
+#find busybox-1.18.5/ -name "*.time" |while read i; do echo $i; rm $i; done 
 find busybox-1.18.5/ -name "*.c.xml" |while read i; do echo $i; rm $i; done
 find busybox-1.18.5/ -name "*_ifdeftoif.c" |while read i; do echo $i; rm $i; done
 find . -name "console_output.txt" |while read i; do echo$i; rm $i; done
