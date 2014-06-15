7f778cd03f91831e0bf50a66c01ffb0e2174095b
Florian Garbe <f.garbe@gmx.de>
2014-06-15 19:30:40 +0200

    openfeature test

--- transformOne.sh
+++ transformOne.sh
@@ -13,7 +13,7 @@ flags="-U HAVE_LIBDMALLOC -DCONFIG_FIND -U CONFIG_FEATURE_WGET_LONG_OPTIONS -U E
 srcPath="busybox-1.18.5"
 filePath=$(find $sourcePath -name $1.c |head -1)
 echo "filepath: $filePath"
-export partialPreprocFlags="--bdd -x CONFIG_ --include busybox/config.h -I $srcPath/include --featureModelDimacs busybox/featureModel.dimacs --recordTiming --parserstatistics --debugInterface --ifdeftoifstatistics"
+export partialPreprocFlags="--bdd -x CONFIG_ --include busybox/config.h -I $srcPath/include --featureModelDimacs busybox/featureModel.dimacs --recordTiming --parserstatistics --openFeat $path/openfeatures.txt --writePI --debugInterface --ifdeftoifstatistics"
 
 ## Reset output
 echo "Starting process on file $1"
