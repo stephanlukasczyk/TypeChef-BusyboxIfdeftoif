ccf5af153b6ba44bbfe1f909fd2b7f82202f075f
Florian Garbe <fgarbe@fim.uni-passau.de>
2014-06-16 13:29:45 +0200

    prepare script now purges MAIN_EXTERNALLY_VISIBLE and EXTERNALLY_VISIBLE macros from busybox files and header files

--- prepareSources.sh
+++ prepareSources.sh
@@ -12,4 +12,9 @@ srcPath="busybox-1.18.5"
 ## Remove the MAIN_EXTERNALLY_VISIBLE macro from sources files
 filesToProcess|while read i; do
   sed -i 's/ MAIN_EXTERNALLY_VISIBLE;/;/g' $srcPath/$i.c
+  sed -i 's/ EXTERNALLY_VISIBLE;/;/g' $srcPath/$i.c
 done
+
+## Remove the MAIN_EXTERNALLY_VISIBLE macro from .h files
+find busybox-1.18.5/ -type f -name "*.h" | xargs sed -i 's/ MAIN_EXTERNALLY_VISIBLE;/;/g'
+find busybox-1.18.5/ -type f -name "*.h" | xargs sed -i 's/ EXTERNALLY_VISIBLE;/;/g'
